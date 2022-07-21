3ba49e63475b23e4e961cd5047068e93506ef0b9
BastienIRD <98462325+BastienIRD@users.noreply.github.com>
2022-07-22 01:55:43 +0200

    wrapping first function
    Test for pull request, for new geoflow need of a function for local actions.

--- tunaatlas_scripts/pre-harmonization/atlantic_ocean_catch_1deg_1m_ps_tunaatlasiccat_level0__byschool.R
+++ tunaatlas_scripts/pre-harmonization/atlantic_ocean_catch_1deg_1m_ps_tunaatlasiccat_level0__byschool.R
@@ -38,7 +38,7 @@
 # Belize   PS 2009-09-01 2009-10-01  5202006     LS     SKJ         C         MT 15.76
 # Belize   PS 2009-09-01 2009-10-01  5202006     LS     YFT         C         MT  2.65
 # Belize   PS 2009-09-01 2009-10-01  5202008     LS     BET         C         MT  3.60
-
+function(action, entity, config){
 #packages
 if(!require(rtunaatlas)){
   if(!require(devtools)){
@@ -129,4 +129,4 @@ file.rename(from = entity$getJobDataResource(config, filename2), to = output_nam
 entity$addResource("source", path_to_raw_dataset)
 entity$addResource("harmonized", output_name_dataset)
 entity$addResource("codelists", output_name_codelists)
-
+}
